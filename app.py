d8f17ed77c03a493e1f82a6088d1e2ff02e23e13
Laxmi Kant Tiwari <laxmimerit@gmail.com>
2024-04-21 18:33:54 +0530

    added working with cache

--- app.py
+++ app.py
@@ -1,66 +1,38 @@
 import streamlit as st
-import time
+import pandas as pd
+from transformers import pipeline
 
-st.set_page_config(
-    page_title="Cool App",
-    page_icon="🧊",
-    layout="wide", # centered
-    initial_sidebar_state="expanded", # ("auto", "expanded", or "collapsed")
-    menu_items={
-        'Get Help': 'https://www.site.com/help',
-        'Report a bug': "https://www.site.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+# Function to simulate an expensive computation
+# @st.cache_data
+def read_data():
+    df = pd.read_csv("https://github.com/laxmimerit/All-CSV-ML-Data-Files-Download/raw/master/IMDB-Dataset.csv")
+    return df.head()
 
-# Sidebar Configuration
-st.sidebar.title("Page Configuration")# Page Content
-st.title("Working with Layouts and Page Configuration")
-st.sidebar.title("Working with sidebar")
+# Function to store and retrieve data in session state
+def update_session_state():
+    if 'counter' not in st.session_state:
+        st.session_state.counter = 0
 
+    st.write("Counter:", st.session_state.counter)
+    st.session_state.counter += 1
 
-# # Columns
-st.write("## Columns")
-col1, col2 = st.columns(2)
-col1.write("This is column 1.")
-col2.write("This is column 2.")
+# Title
+st.title("Working with Caching and Session State")
 
-## use columns ratio
-col1, col2 = st.columns([3,1])
-col1.write("This is column 1.")
-col2.write("This is column 2.")
+# Example of caching
+st.button("Increment counter")
+result = read_data()
+st.write("Result of expensive computation:", result)
 
-col1.button("Click Me")
-col2.button("Click Me Too")
+# update session state
+update_session_state()
 
-# Container
-st.write("## Container")
-container = st.container(border=True, height=200)
-container.write("This content is inside a container.")
-st.write("This content is outside the container.")
-container.write("Containers are useful for grouping content together.")
+# Load NLP model from Hugging Face using caching. It will not reload model again
+@st.cache_resource
+def load_model():
+    model = pipeline("sentiment-analysis")
+    st.success("Loaded NLP model from Hugging Face!")
+    return model
 
-
-# Form
-st.write("## Form")
-with st.form("my_form"):
-    name = st.text_input("Enter your name")
-    submit_button = st.form_submit_button("Submit")
-if submit_button:
-    st.write("Hello,", name, "!")
-
-# Tabs
-st.write("## Tabs")
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-
-with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+model = load_model()
+st.success("Got model successfully!")
